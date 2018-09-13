dc997ab8c5c0d9a4e7398a703774c7a5616944f2
JonasGredig <jongredig@gmail.com>
2018-09-13 20:57:01 +0200

    added sample inputbox

--- src/ch/jonasgredig/localexpenses/View/MainFrame.java
+++ src/ch/jonasgredig/localexpenses/View/MainFrame.java
@@ -1,13 +1,22 @@
 package ch.jonasgredig.localexpenses.View;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class MainFrame extends JFrame {
 
     public MainFrame() {
-
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(400, 600);
+        setSize(600, 400);
+
+        LayoutManager layout = new BorderLayout();
+        setLayout(layout);
+
+        add(new JLabel("Hallo Welt"));
+
+        JTextField textField = new JTextField();
+        add(textField);
+
 
         setVisible(true);
     }
