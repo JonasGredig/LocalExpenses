f98d85cf4b5a27fa805fb6ddfac89d4b00197df0
JonasGredig <jongredig@gmail.com>
2018-09-13 20:34:24 +0200

    Initial commit

---
+++ src/ch/jonasgredig/localexpenses/View/MainFrame.java
@@ -0,0 +1,15 @@
+package ch.jonasgredig.localexpenses.View;
+
+import javax.swing.*;
+
+public class MainFrame extends JFrame {
+
+    public MainFrame() {
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(400, 600);
+
+        setVisible(true);
+    }
+
+}
