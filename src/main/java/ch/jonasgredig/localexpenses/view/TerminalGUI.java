c8e9fa0536b6e7fdf70832ab9c6d3b6683764754
JonasGredig <jongredig@gmail.com>
2018-09-20 12:56:37 +0200

    added new View -> Show All Payments

--- src/main/java/ch/jonasgredig/localexpenses/view/TerminalGUI.java
+++ src/main/java/ch/jonasgredig/localexpenses/view/TerminalGUI.java
@@ -29,7 +29,7 @@ public class TerminalGUI {
                         reader.readLine();
                         break;
                     case 't':
-                        //TODO
+                        //printAllTagsMenu();
                         break;
                     case 's':
                         //TODO
